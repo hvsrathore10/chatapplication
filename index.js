6aa65f5080257bf9439e9983136f7f86b5b037a0
Harshvardhan <harshvardhan8991@gmail.com>
2023-12-31 11:04:06 +0530

    Horizontal Scaling

--- index.js
+++ index.js
@@ -3,67 +3,94 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import dotenv from 'dotenv'
+
 //SQL Lite Database modules ::
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
-// open the database file
-const db = await open({
-  filename: 'chat.db',
-  driver: sqlite3.Database
-});
+//modules help to use multiple core's of OS for Horizontal scaling (also known as "scaling out")
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
-// create our 'messages' table 
-await db.exec(`
+
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  // create one worker per available core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i
+    });
+  }
+
+  // set up the adapter on the primary thread
+  setupPrimary();
+} else {
+  // open the database file
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  });
+
+  // create our 'messages' table 
+  await db.exec(`
   CREATE TABLE IF NOT EXISTS messages (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       client_offset TEXT UNIQUE,
       content TEXT
   );
 `);
+  dotenv.config();
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  connectionStateRecovery: {}
-});
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, './views/index.html'));
-});
-
-io.on('connection',async (socket) => {
-  socket.on('chat message', async (msg) => {
-    let result;
-    try {
-      // store the message in the database
-      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
-    } catch (e) {
-      // TODO handle the failure
-      console.log('msg not store in db!');
-      return;
-    }
-    // include the offset with the message
-    io.emit('chat message', msg, result.lastID);
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    // set up the adapter on each worker thread
+    adapter: createAdapter()
   });
-  if (!socket.recovered) {
-    // if the connection state recovery was not successful
-    try {
-      await db.each('SELECT id, content FROM messages WHERE id > ?',
-        [socket.handshake.auth.serverOffset || 0],
-        (_err, row) => {
-          socket.emit('chat message', row.content, row.id);
-        }
-      )
-    } catch (e) {
-      // something went wrong
-      console.log('something went wrong');
-    }
-  }
-});
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, './views/index.html'));
+  });
+
+  io.on('connection', async (socket) => {
+    socket.on('chat message', async (msg) => {
+      let result;
+      try {
+        // store the message in the database
+        result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      } catch (e) {
+        // TODO handle the failure
+        console.log('msg not store in db!');
+        return;
+      }
+      // include the offset with the message
+      io.emit('chat message', msg, result.lastID);
+    });
+    if (!socket.recovered) {
+      // if the connection state recovery was not successful
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('chat message', row.content, row.id);
+          }
+        )
+      } catch (e) {
+        // something went wrong
+        console.log('something went wrong');
+      }
+    }
+  });
+
+  // each worker will listen on a distinct port
+  const port = process.env.PORT;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
